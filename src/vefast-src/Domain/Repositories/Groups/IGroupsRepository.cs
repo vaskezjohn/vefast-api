2864dc9257d2a69cf1a8cec672282ca5a53642c5
jonathanquiroga <53585827+jonathanquiroga@users.noreply.github.com>
2021-08-17 21:30:59 -0300

    Repositories
    Actualizacion de los repositorios del DDD

---
+++ src/vefast-src/Domain/Repositories/Groups/IGroupsRepository.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace vefast_src.Domain.Repositories.Groups
+{
+    using vefast_src.Domain.Entities.Groups;
+    public interface IGroupsRepository : IGenericRepository<Groups>
+    {
+        
+    }
+}
---
+++ src/vefast-src/Domain/Repositories/User_group/IUser_groupRepository.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace vefast_src.Domain.Repositories.User_group
+{
+    using vefast_src.Domain.Entities.User_group;
+    public interface IUser_groupRepository : IGenericRepository<User_group>
+    {
+        
+    }
+}
