0296543a219934b8cd74480f460f45324be31fbd
jonathanquiroga <53585827+jonathanquiroga@users.noreply.github.com>
2021-08-18 12:58:34 -0300

    Domain & Infrastructure
    Actualizacion de UI de Domain & Infrastructure

--- ui/vefast-api/Extension/DependencyInjection/DomainService.cs
+++ ui/vefast-api/Extension/DependencyInjection/DomainService.cs
@@ -6,6 +6,14 @@ using System.Threading.Tasks;
 using vefast_src.Domain.Services.Company;
 using vefast_src.Domain.Services.Products;
 using vefast_src.Domain.Services.Stores;
+using vefast_src.Domain.Services.Brands;
+using vefast_src.Domain.Services.Categories;
+using vefast_src.Domain.Services.Attribute_value;
+using vefast_src.Domain.Services.Attributes;
+using vefast_src.Domain.Services.Orders_item;
+using vefast_src.Domain.Services.Orders;
+using vefast_src.Domain.Services.User_group;
+using vefast_src.Domain.Services.Groups;
 
 namespace vefast_api.Extension.DependencyInjection
 {
@@ -17,6 +25,15 @@ namespace vefast_api.Extension.DependencyInjection
             services.AddTransient<ICompanyService, CompanyService>();
             services.AddTransient<IProductsService, ProductsService>();
             services.AddTransient<IStoresService, StoresService>();
+            services.AddTransient<IBrandsService, BrandsService>();
+            services.AddTransient<ICategoriesService, CategoriesService>();
+            services.AddTransient<IAttribute_valueService, Attribute_valueService>();
+            services.AddTransient<IAttributesService, AttributesService>();
+            services.AddTransient<IOrders_itemService, Orders_itemService>();
+            services.AddTransient<IOrdersService, OrdersService>();
+            services.AddTransient<IUser_groupService, User_groupService>();
+            services.AddTransient<IGroupsService, GroupsService>();
+
             return services;
         }
     }
--- ui/vefast-api/Extension/DependencyInjection/Infrastructure.cs
+++ ui/vefast-api/Extension/DependencyInjection/Infrastructure.cs
@@ -9,6 +9,22 @@ using vefast_src.Domain.Repositories.Products;
 using vefast_src.Infrastructure.Repositories.Products;
 using vefast_src.Domain.Repositories.Stores;
 using vefast_src.Infrastructure.Repositories.Stores;
+using vefast_src.Domain.Repositories.Brands;
+using vefast_src.Infrastructure.Repositories.Brands;
+using vefast_src.Domain.Repositories.Categories;
+using vefast_src.Infrastructure.Repositories.Categories;
+using vefast_src.Domain.Repositories.Attribute_value;
+using vefast_src.Infrastructure.Repositories.Attribute_value;
+using vefast_src.Domain.Repositories.Attributes;
+using vefast_src.Infrastructure.Repositories.Attributes;
+using vefast_src.Domain.Repositories.Orders_item;
+using vefast_src.Infrastructure.Repositories.Orders_item;
+using vefast_src.Domain.Repositories.Orders;
+using vefast_src.Infrastructure.Repositories.Orders;
+using vefast_src.Domain.Repositories.User_group;
+using vefast_src.Infrastructure.Repositories.User_group;
+using vefast_src.Domain.Repositories.Groups;
+using vefast_src.Infrastructure.Repositories.Groups;
 
 
 namespace vefast_api.Extension.DependencyInjection
@@ -27,6 +43,14 @@ namespace vefast_api.Extension.DependencyInjection
             services.AddTransient<ICompanyRepository, CompanyRepositoryEF>();
             services.AddTransient<IProductsRepository, ProductsRepositoryEF>();
             services.AddTransient<IStoresRepository, StoresRepositoryEF>();
+            services.AddTransient<IBrandsRepository, BrandsRepositoryEF>();
+            services.AddTransient<ICategoriesRepository, CategoriesRepositoryEF>();
+            services.AddTransient<IAttribute_valueRepository, Attribute_valueRepositoryEF>();
+            services.AddTransient<IAttributesRepository, AttributesRepositoryEF>();
+            services.AddTransient<IOrders_itemRepository, Orders_itemRepositoryEF>();
+            services.AddTransient<IOrdersRepository, OrdersRepositoryEF>();
+            services.AddTransient<IUser_groupRepository, User_groupRepositoryEF>();
+            services.AddTransient<IGroupsRepository, GroupsRepositoryEF>();
 
             return services;
         }
