f457aca6fcda4b9a933a8e1cba78739baceb3323
jonathanquiroga <53585827+jonathanquiroga@users.noreply.github.com>
2021-08-18 13:36:58 -0300

    Data Context
    Actualizacion del Data-Context

--- src/vefast-src/Infrastructure/VefastDataContext.cs
+++ src/vefast-src/Infrastructure/VefastDataContext.cs
@@ -6,6 +6,15 @@ using System.Text;
 using System.Threading.Tasks;
 using vefast_src.Domain.Entities.Company;
 using vefast_src.Domain.Entities.Products;
+using vefast_src.Domain.Entities.Stores;
+using vefast_src.Domain.Entities.Brands;
+using vefast_src.Domain.Entities.Categories;
+using vefast_src.Domain.Entities.Attribute_value;
+using vefast_src.Domain.Entities.Attributes;
+using vefast_src.Domain.Entities.Orders_item;
+using vefast_src.Domain.Entities.Orders;
+using vefast_src.Domain.Entities.User_group;
+using vefast_src.Domain.Entities.Groups;
 
 namespace vefast_src.Infrastructure
 {
@@ -16,6 +25,15 @@ namespace vefast_src.Infrastructure
 
         public DbSet<Company> Company { get; set; }
         public DbSet<Products> Products { get; set; }
+        public DbSet<Stores> Stores { get; set; }
+        public DbSet<Brands> Brands { get; set; }
+        public DbSet<Categories> Categories { get; set; }
+        public DbSet<Attribute_value> Attribute_value { get; set; }
+        public DbSet<Attributes> Attributes { get; set; }
+        public DbSet<Orders_item> Orders_item { get; set; }
+        public DbSet<Orders> Orders { get; set; }
+        public DbSet<User_group> User_group { get; set; }
+        public DbSet<Groups> Groups { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
